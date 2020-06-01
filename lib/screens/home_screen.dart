a211ac8a0b2dbcf998894b5d5486f31b7dd45a0a
kharioki <antolisah@gmail.com>
2020-06-02 01:12:17 +0300

    best of the day card

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
+import 'package:book_app/constants.dart';
+import 'package:book_app/widgets/book_rating.dart';
 import 'package:book_app/widgets/reading_list_card.dart';
+import 'package:book_app/widgets/two_side_rounded_button.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -61,8 +64,121 @@ class HomeScreen extends StatelessWidget {
                 ],
               ),
             ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  RichText(
+                    text: TextSpan(
+                      style: Theme.of(context).textTheme.headline5,
+                      children: [
+                        TextSpan(text: 'Best of the '),
+                        TextSpan(
+                          text: 'day',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  bestOfTheDayCard(size, context),
+                ],
+              ),
+            ),
           ],
         ),
+      ),
+    );
+  }
+
+  Container bestOfTheDayCard(Size size, BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 20),
+      width: double.infinity,
+      height: 205,
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              padding: EdgeInsets.only(
+                left: 24,
+                top: 24,
+                right: size.width * 0.35,
+              ),
+              height: 185,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Color(0xFFEAEAEA).withOpacity(0.45),
+                borderRadius: BorderRadius.circular(29),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'New York Time Best For 11th March 2020',
+                    style: TextStyle(
+                      fontSize: 9,
+                      color: kLightBlackColor,
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    'How To Win \nFriends & Influence',
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  Text(
+                    'Gary Venchuk',
+                    style: TextStyle(color: kLightBlackColor),
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    children: <Widget>[
+                      BookRating(score: 4.9),
+                      SizedBox(width: 10),
+                      Expanded(
+                        child: Text(
+                          'When the earth was flat and everyone wanted to win the game of the best and people... .',
+                          maxLines: 3,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                            fontSize: 10,
+                            color: kLightBlackColor,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            right: 0,
+            top: 0,
+            child: Image.asset(
+              'assets/book-3.png',
+              width: size.width * 0.37,
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            child: SizedBox(
+              height: 40,
+              width: size.width * 0.3,
+              child: TwoSideRoundedButton(
+                text: 'Read',
+                radius: 24,
+                press: () {},
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
