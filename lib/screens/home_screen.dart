47f9bb59a3471f6a3e82be871d4adce8bc168fb6
kharioki <antolisah@gmail.com>
2020-06-02 00:11:02 +0300

    reading list widget

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,4 @@
-import 'package:book_app/constants.dart';
-import 'package:book_app/widgets/book_rating.dart';
-import 'package:book_app/widgets/two_side_rounded_button.dart';
+import 'package:book_app/widgets/reading_list_card.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -54,142 +52,17 @@ class HomeScreen extends StatelessWidget {
                     rating: 4.8,
                   ),
                   ReadingListCard(
-                    image: 'assets/book-1.png',
-                    title: 'Crushing & Influence',
-                    author: 'Gary Venchuk',
-                    rating: 4.8,
+                    image: 'assets/book-2.png',
+                    title: 'Top Ten Business Hacks',
+                    author: 'Herman Joel',
+                    rating: 4.7,
                   ),
+                  SizedBox(width: 30),
                 ],
               ),
             ),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class ReadingListCard extends StatelessWidget {
-  final String image;
-  final String title;
-  final String author;
-  final double rating;
-  final Function pressDetails;
-  final Function pressRead;
-
-  const ReadingListCard({
-    Key key,
-    this.image,
-    this.title,
-    this.author,
-    this.rating,
-    this.pressDetails,
-    this.pressRead,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(left: 24, bottom: 40),
-      height: 245,
-      width: 202,
-      child: Stack(
-        children: <Widget>[
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Container(
-              height: 221,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(29),
-                boxShadow: [
-                  BoxShadow(
-                    offset: Offset(0, 10),
-                    blurRadius: 33,
-                    color: kShadowColor,
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Image.asset(
-            image,
-            width: 150,
-          ),
-          Positioned(
-            top: 35,
-            right: 10,
-            child: Column(
-              children: <Widget>[
-                IconButton(
-                  icon: Icon(
-                    Icons.favorite_border,
-                  ),
-                  onPressed: () {},
-                ),
-                BookRating(
-                  score: rating,
-                ),
-              ],
-            ),
-          ),
-          Positioned(
-            top: 160,
-            child: Container(
-              height: 85,
-              width: 202,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(left: 24.0),
-                    child: RichText(
-                      maxLines: 2,
-                      text: TextSpan(
-                          style: TextStyle(color: kBlackColor),
-                          children: [
-                            TextSpan(
-                              text: '$title\n',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            TextSpan(
-                              text: author,
-                              style: TextStyle(
-                                color: kLightBlackColor,
-                              ),
-                            ),
-                          ]),
-                    ),
-                  ),
-                  Spacer(),
-                  Row(
-                    children: <Widget>[
-                      GestureDetector(
-                        onTap: pressDetails,
-                        child: Container(
-                          width: 101,
-                          padding: EdgeInsets.symmetric(vertical: 10),
-                          alignment: Alignment.center,
-                          child: Text('Details'),
-                        ),
-                      ),
-                      Expanded(
-                        child: TwoSideRoundedButton(
-                          text: 'Read',
-                          press: pressRead,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          )
-        ],
       ),
     );
   }
